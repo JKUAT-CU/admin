8b2f8c00970ddb5b78250640437df4cf65c5cddf
Odingoii <okoromaloba@gmail.com>
2024-12-11 17:22:40 +0300

    Correcting ujinga 12345

--- sendtomail.php
+++ sendtomail.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'mail.jkuatcu.org';                     // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'sender@jkuatcu.org';                   // SMTP username
+        $mail->Username   = 'reset@jkuatcu.org';                   // SMTP username
         $mail->Password   = '8&+cqTnOa!A5';                         // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Enable implicit TLS encryption
         $mail->Port       = 465;                                    // TCP port to connect to
