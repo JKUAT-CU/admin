02a0b6ad329bf2f8814b0f618aba33437e1bd436
Odingoii <okoromaloba@gmail.com>
2025-01-18 15:58:44 +0300

    Changing CORS policy

--- api.php
+++ api.php
@@ -4,7 +4,8 @@ header('Content-Type: application/json');
 // Dynamically allow specific origins
 $allowed_origins = [
     'https://ctgo69pcu6mm4wjk.vercel.app',
-    'https://v0.dev/chat/final-portal-UfXVhMMq6kv'
+    'https://v0.dev/chat/final-portal-UfXVhMMq6kv',
+    'https://ctgo69pcu6mm4wjk.vercel.app/'
 ];
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
