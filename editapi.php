24d8453dc7b2a6ed22bdaa7b77e6b1c75e8f9e5a
Odingoii <okoromaloba@gmail.com>
2025-01-22 02:10:57 +0300

    Change of endpoints

--- editapi.php
+++ editapi.php
@@ -1,15 +1,25 @@
 <?php
-
 header('Content-Type: application/json');
 
-// Allow all origins
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+// Allowed origins for CORS
+$allowedOrigins = [
+    'https://admin.jkuatcu.org',
+];
 
-// Handle preflight requests
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Credentials: true');
+} else {
+    http_response_code(403); // Forbidden
+    echo json_encode(['message' => 'Origin not allowed']);
+    exit;
+}
+
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204); // No Content
+    http_response_code(200);
     exit;
 }
 
