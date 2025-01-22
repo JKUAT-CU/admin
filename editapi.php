b5dff31aead6f3eeb0a90cd0fd685ee8dcd24f72
Odingoii <okoromaloba@gmail.com>
2025-01-22 08:04:46 +0300

    Change of endpoints

--- editapi.php
+++ editapi.php
@@ -1,27 +1,27 @@
 <?php
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-// // Allowed origins for CORS
-// $allowedOrigins = [
-//     'https://admin.jkuatcu.org',
-// ];
+// Allowed origins for CORS
+$allowedOrigins = [
+    'https://admin.jkuatcu.org',
+];
 
-// if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-//     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-//     header('Access-Control-Allow-Credentials: true');
-// } else {
-//     http_response_code(403); // Forbidden
-//     echo json_encode(['message' => 'Origin not allowed']);
-//     exit;
-// }
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Credentials: true');
+} else {
+    http_response_code(403); // Forbidden
+    echo json_encode(['message' => 'Origin not allowed']);
+    exit;
+}
 
-// header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-// header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
-// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-//     http_response_code(200);
-//     exit;
-// }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // Database connection
 $mysqli = require_once 'db.php';
