52eb299a2a27d3f85f2c6cbb3a406dce36e8a271
Odingoii <okoromaloba@gmail.com>
2025-01-22 05:48:12 +0300

    Change of endpoints

--- publicity.php
+++ publicity.php
@@ -1,4 +1,27 @@
 <?php
+header('Content-Type: application/json');
+
+// Allowed origins for CORS
+$allowedOrigins = [
+    'https://admin.jkuatcu.org',
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Credentials: true');
+} else {
+    http_response_code(403); // Forbidden
+    echo json_encode(['message' => 'Origin not allowed']);
+    exit;
+}
+
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 require 'publicitydb.php'; // Include your database connection
 
 // Mapping of account numbers to department names
