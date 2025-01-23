0b890f4ad2d62814977239b45ad65599a8d47a13
Odingoii <okoromaloba@gmail.com>
2025-01-23 22:03:21 +0300

    Change of endpoints

--- editbudget.php
+++ editbudget.php
@@ -103,7 +103,7 @@ function fetchBudgetsByDepartmentAndSemester($departmentId, $semester, $conn) {
                 JSON_OBJECT(
                     'event_id', e.id, 
                     'event_name', e.name, 
-                    'expected_attendees', e.attendees, -- Replace 'e.expected_attendees' with the correct column name
+                    'ettendance', e.attendees, -- Replace 'e.expected_attendees' with the correct column name
                     'event_total_cost', e.total_cost
                 )
             ) AS events,
